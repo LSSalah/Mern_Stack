93dc719af60855d9d14de1814caf6f3725606a48
John Doe <johndoe@example.com>
2021-02-14 13:43:00 +0200

    Added TeamManager

---
+++ FullStackMern/TeamManager/client/src/components/AddPlayer.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {navigate} from '@reach/router';
+import axios from 'axios';
+import PlayerForm from './PlayerForm';
+import TopHead from './TopHeader';
+
+export default props => {
+    const {player, setPlayer} = props;
+    const [errors, setErrors] = useState([]);
+
+    const [newPlayer, setNewPlayer] = useState({
+        name: "",
+        position: "",
+        status: {
+            game1: 0,
+            game2: 0,
+            game3: 0,
+        }
+    });
+
+    const addNewPlayer = (newPlayers) => {
+        axios.post(`http://localhost:8000/api/player`, newPlayers)
+        .then(res => {
+            setPlayer([
+                ...player,
+                res.data,
+                navigate(`/players/list`)
+            ])
+        })
+        .catch(err => {
+            const errorRes = err.response.data.errors;
+            const errorArr =[];
+            for(var key of Object.keys(errorRes)){
+                errorArr.push(errorRes[key].message)
+            }
+            setErrors(errorArr)
+        })
+    }
+
+    return (
+        <div>
+            <TopHead/>
+            <div>
+                <h3>Add Player</h3>
+                <PlayerForm newPlayer={newPlayer}
+                setNewPlayer={setNewPlayer}
+                onSubmitProp={addNewPlayer} />
+                {errors.map((err,idx) => <p key={idx}>{err}</p>)}
+            </div>
+        </div>
+    )
+}
